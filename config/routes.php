b9318079fee9c8535992c7f9752912ba3fe9b1b5
Mohammad Alipour <md.alipour91@gmail.com>
2021-12-17 06:23:23 +0330

    edit readme file

--- config/routes.php
+++ config/routes.php
@@ -7,15 +7,15 @@ use App\Presentation\Api\Rest\Controller\Company\CompanyCreateController;
 
 return static function (RoutingConfigurator $routes) {
     //company
-    $routes->add('create_company', 'company')
+    $routes->add('create_company', 'api/v1/company')
         ->controller(CompanyCreateController::class)
         ->methods(['POST']);
 
     //invoice
-    $routes->add('create_invoice', 'invoice')
+    $routes->add('create_invoice', 'api/v1/invoice')
         ->controller(InvoiceCreateController::class)
         ->methods(['POST']);
-    $routes->add('invoice_paid_status', 'invoice/paid')
+    $routes->add('invoice_paid_status', 'api/v1/invoice/paid')
         ->controller(InvoicePaidStatusController::class)
         ->methods(['PUT']);
 };
